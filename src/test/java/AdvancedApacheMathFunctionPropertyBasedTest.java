b54d0a10570a1b8969bb752ae3d6083515f0eefb
Marianna <pincomarianna@gmail.com>
2024-06-24 11:14:32 +0200

    correzione test falliti

--- src/test/java/AdvancedApacheMathFunctionPropertyBasedTest.java
+++ src/test/java/AdvancedApacheMathFunctionPropertyBasedTest.java
@@ -4,6 +4,9 @@ import net.jqwik.api.statistics.Histogram;
 import net.jqwik.api.statistics.StatisticsReport;
 import org.example.AdvancedApacheMathFunction;
 
+import java.util.Arrays;
+import java.util.Random;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class AdvancedApacheMathFunctionPropertyBasedTest {
@@ -23,13 +26,22 @@ public class AdvancedApacheMathFunctionPropertyBasedTest {
     @Property
     @Report(Reporting.GENERATED)
     @StatisticsReport(format = Histogram.class)
-    public void testNonNegativity(@ForAll @Size(min = 1, max = 10000) double[] array1, @ForAll @Size(min = 1, max = 10000) double[] array2) {
-        // Assicura che gli array abbiano almeno un elemento e siano della stessa lunghezza
-        Assume.that(array1.length > 0 && array2.length > 0 && array1.length == array2.length);
+    public void testNonNegativity(@ForAll @Size(min = 1, max = 10000) double[] array1) {
+        // Generare array2 con la stessa lunghezza di array1
+        double[] array2 = new double[array1.length];
+        Random random = new Random();
+        for (int i = 0; i < array2.length; i++) {
+            array2[i] = random.nextDouble();
+        }
+
+        // Stampare gli array per il debug
+        System.out.println("array1: " + Arrays.toString(array1));
+        System.out.println("array2: " + Arrays.toString(array2));
 
         double result = AdvancedApacheMathFunction.geometricMeanOfSumOfSquares(array1, array2);
-        assertTrue(result >= -1e-10, "La media geometrica deve essere non negativa");
+        assertTrue(result >= 0, "La media geometrica deve essere non negativa");
     }
+
 
 
     @Property
